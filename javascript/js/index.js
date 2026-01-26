2cc1ea5339ec9d48eb4bc5780df8f45ef92d917f
Joel Ross <joelross@uw.edu>
2026-01-26 12:20:00 -0800

    End of lecture demo

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,2 +1,42 @@
 'use strict';
 
+console.log("Hello world");
+console.log("Go hawks!");
+
+// const x = "hello";
+// x = 42;
+// console.log(x);
+// let hoursSlept;
+// console.log(hoursSlept);
+
+console.log('404' - 4);
+
+const num = 10
+const str = '10'
+
+const areSame = ('' == false)
+console.log(areSame);
+
+const lettersArray = ['a', 'b', 'c'];
+
+
+lettersArray[5] = 'f';
+
+console.log(lettersArray);
+console.log(lettersArray[4]);
+
+const agesObj = {sarah:42, amit:35, zhang:13}
+console.log(agesObj);
+
+const numWords = {1:'one', 2:'two', 3:'three'}
+console.log(numWords);
+
+const objectsKeys = Object.keys(numWords);
+console.log(objectsKeys);
+
+agesObj['fred'] = 19;
+console.log(agesObj);
+
+console.log(agesObj.joel);
+
+console.log(agesObj[fred]);
