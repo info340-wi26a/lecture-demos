a1d14ef278dcc45251744a7410c3f3d5c8399bd5
Joel Ross <joelross@uw.edu>
2026-01-27 15:10:01 -0800

    Add functional javascript starter code

---
+++ javascript-functional/js/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//an array of objects to work with
+const peopleArray = [
+  {name: 'Ada', height: 64, weight: 135},
+  {name: 'Bob', height: 74, weight: 156},
+  {name: 'Chris', height: 69, weight: 139, pronouns: 'they/them'},
+  {name: 'Diya', height: 69, weight: 144},
+  {name: 'Emma', height: 71, weight: 152}
+]
+console.log(peopleArray);
