26c6f8ffb7df8241fb1e73cd1e4064e18d4decb6
Joel Ross <joelross@uw.edu>
2026-02-04 12:45:42 -0800

    End of es6 demo

--- es6/js/index.js
+++ es6/js/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
-//console.log("executing main.js")
+import { favoriteClass } from './other.js';
+
+import printName from './other.js';
+
+
+console.log("executing main.js")
 
 //an array of objects to work with
 const peopleArray = [
@@ -11,3 +16,39 @@ const peopleArray = [
   {name: 'Emma', height: 71, weight: 152}
 ]
 //console.log(peopleArray);
+
+
+peopleArray.forEach(printName);
+
+
+const foo = (params) => { 
+  return 'foo '+params;
+}
+
+console.log(foo('world'));
+
+// const myArray = [1, 2, 3];
+// const [x, y, z, q] = myArray;
+// // const x = myArray[0];
+// // const y = myArray[1];
+// // const z = myArray[2];
+// console.log(x); //=> 1;
+// console.log(y); //=> 2;
+// console.log(z); //=> 3;
+// console.log(q);
+
+
+//destructuring objects
+// const myObject = {a: 1, b: 2, c: 3, e: 5};
+// const {b, a, c, d} = myObject; //myObject.a to a, etc.
+// console.log(a); //=> 1
+// console.log(b); //=> 2;
+// console.log(c); //=> 3;
+// console.log(d); 
+
+// const getBMI = ({height, weight}) => 703*weight/(height*height); 
+
+function getBMI(personObj) {
+  const {height, weight} = person;
+  return 703*weight/(height*height);
+}
--- es6/js/other.js
+++ es6/js/other.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log("executing other.js");
+console.log("Hello world");
+
+const x = 340;
+const peopleArray = ["John", "Paul", "George", 'Ringo'];
+console.log("other people!!", peopleArray);
+
+export default function printName(personObj){
+  console.log(personObj.name);
+}
+
+export const favoriteClass = "INFO 340";
